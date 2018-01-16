ca6ccf54fbf4b698de35066970862152ccc81279
David Dassau <todieahero@hotmail.com>
2018-01-15 20:50:17 -0600

    created some children

--- LoopingConsole/Program.cs
+++ LoopingConsole/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LoopingConsole
 {
@@ -10,6 +7,29 @@ namespace LoopingConsole
     {
         static void Main(string[] args)
         {
+
+            var children = new List<Child>
+            {
+                new Child { Name = "Suzy", Sick = false },
+                new Child { Name = "Ralph", Sick = true }
+            };
+
+            //for (var i = 0; i < children.Count - 1; i++)
+            //{
+            //    var child = children[i];
+            //    Console.WriteLine($"{child.Name} is {(child.Sick ? "sick" : "healthy")}");
+            //    Console.WriteLine(child.ForgeASickNote());
+            //}
+
+            foreach (var child in children)
+            {
+                Console.WriteLine($"{child.Name} is {(child.Sick ? "sick" : "healthy")}");
+                Console.WriteLine(child.ForgeASickNote());
+            }
+
+            Console.WriteLine("This is our second time talking about the same thing.");
+
+            var lineIRead = Console.ReadLine();
         }
     }
 }
